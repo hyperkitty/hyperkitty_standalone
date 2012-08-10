81eb676ffa11be2744f5734f1a6ad5379eb1cb18
Aamir Khan <syst3m.w0rm@gmail.com>
2012-08-10 19:03:06 +0530

    Login URL fixed

--- settings.py
+++ settings.py
@@ -154,9 +154,9 @@ INSTALLED_APPS = (
 )
 
 
-LOGIN_URL          = '/accounts/login/'
+LOGIN_URL          = '/hyperkitty/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGIN_ERROR_URL    = '/accounts/login/'
+LOGIN_ERROR_URL    = '/hyperkitty/accounts/login/'
 SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
 SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
 SOCIAL_AUTH_DEFAULT_USERNAME = 'new_social_auth_user'
