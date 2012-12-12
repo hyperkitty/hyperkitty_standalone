9112776bb2ee2df7c40df4d4f051dd977de0549e
Aurélien Bompard <aurelien@bompard.org>
2012-12-12 11:22:25 +0100

    Make the request object available in templates

--- settings.py
+++ settings.py
@@ -111,6 +111,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.csrf",
+    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
     "hyperkitty.context_processors.app_name",
 )
