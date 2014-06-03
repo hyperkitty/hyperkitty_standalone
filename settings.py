d6c8f08f860dbac7d6b4353d05bc99bcb2a82db8
Aurélien Bompard <aurelien@bompard.org>
2014-06-03 11:37:19 +0200

    Use django-discover-runner on Django < 1.6

--- settings.py
+++ settings.py
@@ -173,6 +173,11 @@ INSTALLED_APPS = (
     'django_browserid',
 )
 
+import django
+if django.VERSION[:2] < (1, 6):
+    TEST_RUNNER = 'discover_runner.DiscoverRunner'
+
+
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
