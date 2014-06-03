101514e5a36f814bad15d079b38186cb5ac33af8
Aurélien Bompard <aurelien@bompard.org>
2014-06-03 11:25:09 +0200

    BrowserID auth is handled by django-browserid

--- settings.py
+++ settings.py
@@ -145,7 +145,6 @@ TEMPLATE_DIRS = (
 )
 
 AUTHENTICATION_BACKENDS = (
-    'social_auth.backends.browserid.BrowserIDBackend',
     'social_auth.backends.google.GoogleBackend',
     'social_auth.backends.yahoo.YahooBackend',
     #'social_auth.backends.OpenIDBackend',
