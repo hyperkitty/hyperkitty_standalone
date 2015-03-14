1a98b264b1378a9de8a72a87813f69ae4721d263
Aurélien Bompard <aurelien@bompard.org>
2015-03-14 13:25:03 +0100

    Typo

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ BROWSERID_AUDIENCES = [ "http://localhost", "http://localhost:8000" ]
 MAILMAN_REST_SERVER = 'http://localhost:8001'
 MAILMAN_API_USER = 'restadmin'
 MAILMAN_API_PASS = 'restpass'
-MAILMAN_ARCHIVER_KEY = 'SecretArchiveAPIKey'
+MAILMAN_ARCHIVER_KEY = 'SecretArchiverAPIKey'
 MAILMAN_ARCHIVER_FROM = ('127.0.0.1', '::1')
 
 # Application definition
