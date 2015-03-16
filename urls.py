8c01febd2051579eb22672ccafe9f6e33ccf3627
Aurélien Bompard <aurelien@bompard.org>
2015-03-16 17:44:57 +0100

    Cleanup urls.py

--- urls.py
+++ urls.py
@@ -23,22 +23,18 @@ HyperKitty, just set the ROOT_URLCONF to "hyperkitty.urls" in your settings.py
 configuration file.
 """
 
-import hyperkitty
-
 from django.conf.urls import patterns, include, url
-from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
 
+# Comment the next two lines to disable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns('',
     url(r'^$', RedirectView.as_view(url=reverse_lazy('hyperkitty.views.index.index'))),
+    #url(r'^postorius/', include('postorius.urls')),
     url(r'^hyperkitty/', include('hyperkitty.urls')),
-    url(r'^postorius/', include('postorius.urls')),
     url(r'', include('social_auth.urls'), {"SSL": True}),
     url(r'', include('django_browserid.urls'), {"SSL": True}),
 )
